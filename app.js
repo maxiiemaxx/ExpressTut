6f50a2b7afc1178f39c964454863bd76e63942c4
Max van Kuik <maxvankuik@mbp-van-max-8.home>
2015-12-04 19:53:13 +0100

    initial commit

---
+++ app.js
@@ -0,0 +1,14 @@
+var express = require('express');
+
+var app = express();
+ 
+app.set("port", process.env.PORT || 3000);
+
+
+app.get('/', function (req, res) {
+  res.send('Express is aan hello World');
+});
+
+app.listen(app.get("port"), function() {
+	console.log("TO TERMINATE DOEI");
+ });
